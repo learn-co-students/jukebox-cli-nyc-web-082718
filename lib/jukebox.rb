4a3197e3e9e5f232897748f62ef8cfc14c7d0e07
github username <github email address>
2018-08-23 00:16:54 -0400

    create a simple CLI jukebox

--- lib/jukebox.rb
+++ lib/jukebox.rb
@@ -9,4 +9,56 @@ songs = [
   "Harry Chapin - Cats in the Cradle",
   "Amos Lee - Keep It Loose, Keep It Tight"
 ]
+def help
+  puts "I accept the following commands:"
+  puts "- help : displays this help message"
+  puts "- list : displays a list of songs you can play"
+  puts "- play : lets you choose a song to play"
+  puts "- exit : exits this program"
+end
 
+def list(songs)
+  songs.each_with_index do |song, index|
+    puts "#{index + 1}. #{song}"
+  end
+end
+
+def play(songs)
+  puts "Please enter a song name or number: "
+  play_song = gets.chomp
+  if songs.include?(play_song)
+    puts "Playing #{play_song}"
+  elsif (1..9).include?(play_song.to_i)
+    puts "Playing #{songs[play_song.to_i-1]}"
+  else
+    puts "Invalid input, please try again"
+  end
+end
+
+def exit_jukebox
+  puts "Goodbye"
+end
+
+def run(songs)
+  help
+
+  command = ""
+  while command
+    puts "Please enter a command:"
+    command = gets.chomp
+    case command
+      when "list"
+        list(songs)
+      when "play"
+        play(songs)
+      when "help"
+        help
+      when "exit"
+        exit_jukebox
+        break
+      else
+        help
+      end
+    end
+end
+binding.pry
